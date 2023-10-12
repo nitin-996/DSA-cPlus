785c95982d3ba35321a36e1325d3435b2129a186
torrento <sachinles99@gmail.com>
2023-10-12 18:26:50 +0530

    added

---
+++ array.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+
+//     // initialising an array.
+//     int numbers[10] = {15, 45, 78, 69, 36, 45, 25, 55};
+
+//     int n;
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+
+//     {
+//         int write = numbers[i];
+
+//         cout << write << endl;
+//     }
+// }
+
+
+// ###################################################################################
+
+// initialize all array with same value
+
+
+  
+// int main(void) 
+// { 
+//     int size = 50; 
+//     int val; 
+//     cin>>val;
+  
+//     // array declaration 
+//     int arr[size]; 
+//     int i; 
+  
+//     // initializing array elements 
+//     for (i = 0; i < size ; i++){ 
+//         arr[i] = val; 
+//     } 
+  
+//      // print array 
+     
+//      for (i = 0; i < size ; i++){ 
+//         cout<<arr[i]; 
+//     } 
+  
+//     return 0; 
+// } 
+
+// ##################################################################################################
+
+// array with is_function
+
+
+
+void printarray(int arr[], int size){
+
+
+    
+
+
+
+}
+
+int main(){
+
+
+
+
+    
+}
