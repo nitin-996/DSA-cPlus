cf22b0d3297691a12b591c781ae26996293312f5
torrento <sachinles99@gmail.com>
2023-10-20 18:54:08 +0530

    added

--- array.cpp
+++ array.cpp
@@ -331,4 +331,4 @@ int main() {
     printArray(arr, 8);
 
     return 0;
-}
+}  
---
+++ binarySearch.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+using namespace std;
+
+int binarysearch(int arr[], int size, int key){
+    int start = 0;
+    int end = size-1;
+
+    int mid = start + (end-start)/2;
+
+    while(start <=end){
+        if(arr[mid] == key)
+{
+    return mid;
+}   
+
+if (key > arr[mid]){
+    start = mid +1;
+
+}else{
+    end = mid-1;
+}
+
+mid = start + (end-start)/2;
+
+
+ }
+
+ return -1;
+
+}
+
+
+int main (){
+
+    int even[6]={2,4,6,8,12,18};
+    int odd[5]={3,5,7,8,11};
+
+    int index = binarysearch(even , 6 ,12);
+
+    cout<< "even"<< index;
+
+     int index = binarysearch(odd , 5, 11);
+    
+    cout<< "odd"<< odd;
+
+
+
+}
